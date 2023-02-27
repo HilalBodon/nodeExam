92018e2ffe4104384d0f04233cf33f54848f6eab
HilalBodon <hilal_bodon@hotmail.com>
2023-02-27 22:00:07 +0200

    removing node_modules

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const express = require("express");
+const app = express();
 const DB = require("./database").connectDB;
 
 DB();
-// to use json 
+app.use(express.json());
+app.use('/api/auth',authRouter);
 
 app.listen(process.env.PORT,()=>{
     console.log(`Listening on port: ${process.env.PORT}`);
